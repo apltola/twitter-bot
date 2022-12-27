4b14494cd76ddc59ad2469f76047957357ad7ec3
Aleksi Peltola <aleksijaakkojuhani@gmail.com>
2022-12-27 10:46:36 +0200

    update util.ts
    elaborate more on comment

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -2,8 +2,8 @@ import fs from 'fs';
 
 /**
  * This is kinda hacky but whatever.
- * In production, get base64 encoded GCP service account key, and create a key_file.json
- * which gives the app access to Firestore
+ * In production, get base64-encoded GCP service account key fron environment variable,
+ * and create a key_file.json which gives the app access to Firestore
  */
 export const makeKeyFile = () => {
   if (process.env.NODE_ENV !== 'production') return;
